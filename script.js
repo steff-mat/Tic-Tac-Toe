d47a96ab8d1b85bd2eeb2d13f29a01fac8490ac0
Stefan <107488671+steff-mat@users.noreply.github.com>
2023-04-18 03:56:00 +0200

    draft 3

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+let gameState = true;
+
 const dom = {
   form: document.querySelector('form'),
   startMenu: document.querySelector('.initial-state'),
@@ -8,22 +10,28 @@ const dom = {
   restartButton: document.querySelector('.restart'),
   nameInput: document.querySelector('#username'),
   buttons: document.querySelectorAll('.box'),
+  winner: document.querySelector('.winner-confirmation'),
   nameHolder: [],
 };
 
-const xo = {
+function toggler(x) {
+  return x.classList.toggle('hidden');
+}
+
+dom.form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  dom.nameHolder.push(dom.nameInput.value);
+  dom.form.reset();
+  toggler(dom.startMenu);
+  toggler(dom.gameMenu);
+});
+
+const select = {
   x: 'X',
   o: 'O',
-};
-
-const winner = {
-  win: 'Somebody won!',
-  noWin: 'Draw!',
-};
-
-const selection = {
   player: [],
   cpu: [],
+  moveCount: 0,
 };
 
 const combos = [
@@ -38,16 +46,56 @@ const combos = [
 ];
 
 function playerLogic() {
-  for (const button of dom.buttons) {
-    button.addEventListener('click', () => {
-      if (selection.player.includes(event.target.id) === false) {
-        button.innerText = xo.x;
-        selection.player.push(event.target.id);
-      }
-    });
+  if (gameState === true) {
+    for (const button of dom.buttons) {
+      button.addEventListener('click', () => {
+        if (select.player.includes(event.target.id) === false) {
+          button.innerText = select.x;
+          select.player.push(parseInt(event.target.id));
+          select.moveCount++;
+          loopCombo(select.player, combos);
+          if (select.moveCount <= 8) {
+            cpuLogic();
+          }
+        }
+      });
+    }
   }
 }
 playerLogic();
 
-function cpuLogic() {}
-cpuLogic();
+function cpuLogic() {
+  const docLoader = function (x) {
+    if (select.cpu.includes(x) == false && select.player.includes(x) == false) {
+      select.moveCount++;
+      select.cpu.push(x);
+      loopCombo(select.cpu, combos);
+      return (document.getElementById(`${x}`).innerText = select.o);
+    } else {
+      docLoader(xzy());
+    }
+  };
+  const xzy = () => {
+    return Math.floor(Math.random() * 9 + 1);
+  };
+  docLoader(xzy());
+}
+
+function loopCombo(a, b) {
+  for (const arr of b) {
+    const matches = a.filter((item) => arr.includes(item));
+    if (matches.length > 2) {
+      gameState = false;
+      console.log(`At least 3 items in ${a} are present in ${arr}`);
+      toggler(dom.gameMenu);
+      toggler(dom.restartMenu);
+      break;
+    }
+  }
+}
+
+dom.restartButton.addEventListener('click', () => {
+  toggler(dom.restartMenu);
+  toggler(dom.startMenu);
+  location.reload();
+});
