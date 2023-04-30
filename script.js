774570f23c2c62a497f99e26f6a6403a35ba2d75
Stefan Maties <107488671+steff-mat@users.noreply.github.com>
2023-04-30 02:49:17 +0200

    Improve overall layout/design

--- script.js
+++ script.js
@@ -42,6 +42,7 @@ function toggler(x) {
 }
 
 (function () {
+  //Save username to localstorage and keep as default unless changed
   if (localStorage.player !== typeof undefined) {
     document.querySelector('#username').value = localStorage.player;
   }
@@ -79,13 +80,15 @@ function playerLogic() {
           human.selection.includes(parseInt(event.target.id)) === false &&
           button.innerText === ''
         ) {
+          design(button, 'red');
           button.innerText = xType.box;
           human.selection.push(parseInt(event.target.id));
           select.moveCount++;
           loopCombo(human.selection, combos, localStorage.getItem('player'));
           draw();
           if (select.moveCount <= 8 && winnerFound !== true) {
-            cpuLogic();
+            // cpuLogic();
+            setTimeout(cpuLogic, 250);
           }
         }
       });
@@ -95,17 +98,20 @@ function playerLogic() {
 playerLogic();
 
 function cpuLogic() {
+  //Brain for CPU
   const docLoader = function (x) {
+    //Make sure as p/cpu box is not in-use already
     if (
       cpu.selection.includes(x) == false &&
       human.selection.includes(x) == false
     ) {
       select.moveCount++;
       cpu.selection.push(x);
+      design(document.getElementById(`${x}`), 'blue');
       document.getElementById(`${x}`).innerText = oType.box;
       loopCombo(cpu.selection, combos, cpu.name);
     } else {
-      docLoader(xzy());
+      docLoader(xzy()); //Thy to print to box otherwise call again
     }
   };
   const xzy = () => {
@@ -115,7 +121,7 @@ function cpuLogic() {
 }
 
 function loopCombo(a, b, c) {
-  console.log(localStorage.getItem('player'));
+  //Checks if there's a combination match and post winner
   for (const arr of b) {
     const matches = a.filter((item) => arr.includes(item));
     if (matches.length === 3) {
@@ -132,14 +138,23 @@ function loopCombo(a, b, c) {
 }
 
 dom.restartButton.addEventListener('click', () => {
-  location.reload();
+  location.reload(); //Reload webpage for next round
 });
 
 function draw() {
+  //Confirm no winner this round
   if (winnerFound === false && select.moveCount === 9) {
     dom.winner.innerText = 'Draw!';
     gameState = false;
     toggler(dom.gameMenu);
     toggler(dom.restartMenu);
   }
+}
+
+function design(tag, color) {
+  //Colorify the game
+  tag.style.color = color;
+  tag.style.fontSize = '6rem';
+  tag.style.fontWeigth = 'boldest';
+  tag.style.transition = 'all 250ms';
 }
